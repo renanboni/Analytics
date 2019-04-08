82089247a2f0fb602233d9bd0160599927b1665b
Renan Boni <renan-boni@hotmail.com>
2019-04-08 18:14:18 -0300

    Implemented some functions on firebase tracker class

---
+++ analytics/src/main/java/com/boni/analytics/firebase/FirebaseEvent.kt
@@ -0,0 +1,5 @@
+package com.boni.analytics.firebase
+
+import android.os.Bundle
+
+class FirebaseEvent(val name: String, val params: Bundle? = null)
--- analytics/src/main/java/com/boni/analytics/firebase/FirebaseTracker.kt
+++ analytics/src/main/java/com/boni/analytics/firebase/FirebaseTracker.kt
@@ -1,19 +1,57 @@
 package com.boni.analytics.firebase
 
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
 import com.boni.analytics.Analytics
+import com.boni.analytics.Event
+import com.google.firebase.analytics.FirebaseAnalytics
 import io.reactivex.disposables.CompositeDisposable
 import javax.inject.Inject
 
-class FirebaseTracker @Inject constructor(private val analytics: Analytics) {
+class FirebaseTracker @Inject constructor(
+    private val context: Context,
+    private val analytics: Analytics
+) {
 
     private val compositeDisposable = CompositeDisposable()
+
+    init {
+        subscribeToEvents()
+    }
 
     private fun subscribeToEvents() {
         compositeDisposable.add(
             analytics.eventStream
-                .subscribe {
-
-                }
+                .map(this::mapToFirebaseEvent)
+                .subscribe({
+                    sendEvent(it)
+                }, {
+                    logError(it.localizedMessage)
+                })
         )
+    }
+
+    private fun mapToFirebaseEvent(e: Event): FirebaseEvent {
+        val firebaseEvent = FirebaseEvent(e.name)
+
+        e.params?.let {
+            val bundle = Bundle()
+
+            for (param in it) {
+                bundle.putString(param.key, param.value as String)
+            }
+        }
+
+        return firebaseEvent
+    }
+
+    private fun logError(err: String) {
+        // do something here, crashlytics or whatever
+        Log.i("FirebaseTracker::Error", err)
+    }
+
+    private fun sendEvent(e: FirebaseEvent) {
+        FirebaseAnalytics.getInstance(context).logEvent(e.name, e.params)
     }
 }
